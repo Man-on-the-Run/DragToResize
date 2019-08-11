580bed49757dea6e5fdc3c58a1945183564d47a6
philman <phil.man.dev@gmail.com>
2019-08-11 12:25:05 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+var sidebar = document.getElementById('sidebar');
+var range = 10;
+
+function getWidth(element) {
+  return parseInt(getComputedStyle(element).getPropertyValue('width'));
+}
+
+function adjustWidth(e) {
+  this.firstElementChild.style.width = e.clientX + 'px';
+  this.classList.add('resizing');
+  this.addEventListener('mouseup', function() {
+    this.removeEventListener('mousemove', adjustWidth);
+    this.classList.remove('resizing');
+    this.firstElementChild.addEventListener('mousemove', highlightEdge);
+  });
+}
+
+function select() {
+  this.removeEventListener('mousemove', highlightEdge);
+  this.parentElement.addEventListener('mousemove', adjustWidth);
+}
+
+function highlightEdge(e) {
+  if(e.clientX >= getWidth(this) - range) {
+    this.classList.add('on-edge');
+    this.addEventListener('mousedown', select);
+  } else {
+    this.classList.remove('on-edge');
+  }
+}
+
+sidebar.addEventListener('mousemove', highlightEdge);
+sidebar.addEventListener('mouseleave', function() {
+  this.classList.remove('on-edge');
+});
